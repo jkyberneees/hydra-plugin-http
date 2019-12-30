000c1d623a92fd49e958112a07dd38c4513b655a
Rolando Santamaria Maso <kyberneees@gmail.com>
2019-12-30 11:25:25 +0100

    updating deps

--- libs/proxy.js
+++ libs/proxy.js
@@ -1,4 +1,4 @@
-/* eslint no-param-reassign:0, no-underscore-dangle:0, no-restricted-syntax:0,
+/* eslint no-param-reassign:0, no-underscore-dangle:0, node/no-deprecated-api:0, no-restricted-syntax:0,
 guard-for-in:0, no-await-in-loop: 0 */
 
 const Route = require('route-parser')
